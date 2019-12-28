420813df2a75eafe742e91cc24488969f0c3c33d
Micah Howard <micahbhoward@me.com>
2019-12-28 07:31:23 -0500

    Render actual messages on home page, instead of hard coded messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Rachel", password: "password")
+User.create(username: "Cash", password: "password")
+User.create(username: "Ethan", password: "password")
+User.create(username: "Sonia", password: "password")
+User.create(username: "Scarlet", password: "password")
+User.create(username: "Prince", password: "password")
+
+Message.create(body: "Second test message", user: User.last)
+Message.create(body: "Third test message", user: User.find(2))
+Message.create(body: "Fourth test message", user: User.find(3))
+Message.create(body: "Fifth test message", user: User.first)
+Message.create(body: "Sixth test message", user: User.find(4))
+
